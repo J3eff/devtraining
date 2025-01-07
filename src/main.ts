f150af73f151ff06e3423b8f70103f0dcb228324
Jefferson Brandão <jeffersonvideo125@gmail.com>
2025-01-07 01:02:04 -0300

    feat: Validando os dados recebidos pelo App - parte 2

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe()); // validação global automatica
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //Remove propriedades que não são esperadas na requisição
+      forbidNonWhitelisted: true, // Recusa requisições com dados adicionais não desejados
+      transform: true, // transforma o payload recebido na requisição para o tipo esperado
+    }),
+  ); // validação global automatica
 
   await app.listen(process.env.PORT ?? 3000);
 }
