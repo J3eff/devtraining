894a00823d7af998f2ad6f557b36fd20788589f2
Jefferson Brandão <jeffersonvideo125@gmail.com>
2025-01-08 01:02:17 -0300

    feat: 31. Refatorando os métodos findAll e findOne para buscar os dados relacionados

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -17,11 +17,14 @@ export class CoursesService {
   ) {}
 
   async findAll() {
-    return await this.coureseRepository.find();
+    return await this.coureseRepository.find({ relations: ['tags'] });
   }
 
   async findOne(id: number) {
-    const course = await this.coureseRepository.findOne({ where: { id } });
+    const course = await this.coureseRepository.findOne({
+      where: { id },
+      relations: ['tags'],
+    });
 
     if (!course) throw new NotFoundException(`Course ID ${id} not found`);
 
