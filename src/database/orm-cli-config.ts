afcf2e07c43578d1aa8a6980cfea11e77a20547e
Jefferson Brandão <jeffersonvideo125@gmail.com>
2025-01-09 12:38:08 -0300

    Criando a tabela tags através de migração

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1736435393976 } from 'src/migrations/1736435393976-CreateCoursesTable';
+import { CreateTagsTable1736436491141 } from 'src/migrations/1736436491141-CreateTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1736435393976],
+  migrations: [CreateCoursesTable1736435393976, CreateTagsTable1736436491141],
 });
